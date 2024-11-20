acf1c0df4b3add07ca0bbcd0a873e2bf3e8f90fa
Jonas Hietala <mail@jonashietala.se>
2024-11-20 15:49:38 +0100

    Refactor rendering

--- src/render.rs
+++ src/render.rs
@@ -1,4 +1,5 @@
 use crate::parse::Combo;
+use crate::parse::Key;
 use crate::parse::Keymap;
 use crate::parse::Layer;
 use crate::render_opts::RenderOpts;
@@ -16,12 +17,12 @@ use std::str::FromStr;
 
 pub fn render(keymap: &Keymap, render_opts: &RenderOpts, output_dir: &Utf8Path) -> Result<()> {
     for layer in keymap.layers.iter() {
-        render_layer(layer, &render_opts, output_dir)?;
+        render_layer(layer, render_opts, output_dir)?;
     }
 
-    render_legend(&render_opts, output_dir)?;
+    render_legend(render_opts, output_dir)?;
 
-    render_combos(&keymap.combos, &render_opts, output_dir)?;
+    render_combos(&keymap.combos, render_opts, output_dir)?;
 
     Ok(())
 }
@@ -30,23 +31,17 @@ fn render_legend(render_opts: &RenderOpts, output_dir: &Utf8Path) -> Result<()> 
     let path = output_dir.join("legend.svg");
     let mut file = File::create(path)?;
 
-    let border = 10.0;
-    let border_w = 6.0;
-    let border_top = 4.0;
-    let border_bottom = border_w * 2.0 - border_top;
-
-    let inner_h = 42.0;
-    let outer_h = inner_h + border_w * 2.0;
-
-    let inner_w = 4.0 * 42.0;
-    let outer_w = inner_w + border_w * 2.0;
+    let keymap_border = 10.0;
+    let key_side = 54.0;
+    let key_w = 4.0 * key_side;
+    let key_h = key_side;
 
     let item_count = render_opts.legend.len();
     let columns = std::cmp::min(item_count, 4);
     let rows = item_count / columns;
 
-    let max_x = columns as f32 * outer_w + border * 2.0;
-    let max_y = rows as f32 * outer_h + border * 2.0;
+    let max_x = columns as f32 * key_w + keymap_border * 2.0;
+    let max_y = rows as f32 * key_h + keymap_border * 2.0;
 
     writeln!(
         file,
@@ -74,38 +69,26 @@ fn render_legend(render_opts: &RenderOpts, output_dir: &Utf8Path) -> Result<()> 
         let col = i - row * columns;
 
         let class = &item.class;
-        let txt = html_escape::encode_safe(&item.title);
+        let txt = &item.title;
 
-        let outer_x = border + col as f32 * outer_w;
-        let outer_y = border + row as f32 * outer_h;
-
-        let inner_x = border + col as f32 * outer_w + border_w;
-        let inner_y = border + row as f32 * outer_h + border_top;
-
-        let text_x = inner_x + inner_w / 2.0;
-        let text_y = inner_y + inner_h / 2.0;
+        let outer_x = keymap_border + col as f32 * key_w;
+        let outer_y = keymap_border + row as f32 * key_h;
 
         let outer_color = render_opts
             .colors
             .get(&item.class)
             .unwrap_or(&fallback_color);
 
-        let inner_color = lighten_color(Srgb::from_str(outer_color).unwrap().into(), 0.05);
-        let inner_color = format!("#{:x}", Srgb::<u8>::from(inner_color));
-
-        writeln!(
-            file,
-            r##"<g class="legend {class}">
-
-      <rect x="{outer_x}" y="{outer_y}"
-            width="{outer_w}" height="{outer_h}"
-            rx="5" fill="{outer_color}" class="outer border"/>
-      <rect x="{inner_x}" y="{inner_y}"
-            width="{inner_w}" height="{inner_h}"
-            rx="5" fill="{inner_color}" class="inner border"/>
-    <text x="{text_x}" y="{text_y}" text-anchor="middle" dominant-baseline="middle">{txt}</text>
-    </g>
-"##,
+        write_bordered_key(
+            &mut file,
+            outer_x,
+            outer_y,
+            key_w,
+            key_h,
+            class,
+            outer_color,
+            txt,
+            None,
         )?;
     }
 
@@ -118,26 +101,17 @@ fn render_layer(layer: &Layer, render_opts: &RenderOpts, output_dir: &Utf8Path) 
     let path = output_dir.join(format!("{}.svg", layer.id.0));
     let mut file = File::create(path)?;
 
-    // TODO
-    // - Calcualate viewbox better from keys
-    // - Need to have a way to deal with keys that activate the current layer
-    // - Customize font size?
-
-    let keyboard_border = 10.0;
-    let border_w = 6.0;
-    let border_top = 4.0;
-
-    let inner_w = 42.0;
-    let outer_w = inner_w + border_w * 2.0;
+    let key_w = 54.0;
+    let keymap_border = 10.0;
 
     let mut max_x: f32 = 0.0;
     let mut max_y: f32 = 0.0;
     for key in layer.keys.iter() {
-        max_x = max_x.max((1.0 + key.x) * outer_w);
-        max_y = max_y.max((1.0 + key.y) * outer_w);
+        max_x = max_x.max((1.0 + key.x) * key_w);
+        max_y = max_y.max((1.0 + key.y) * key_w);
     }
-    max_x += keyboard_border * 2.0;
-    max_y += keyboard_border * 2.0;
+    max_x += keymap_border * 2.0;
+    max_y += keymap_border * 2.0;
 
     writeln!(
         file,
@@ -160,76 +134,41 @@ fn render_layer(layer: &Layer, render_opts: &RenderOpts, output_dir: &Utf8Path) 
         .as_bytes(),
     )?;
 
-    let fallback_color = "#e5c494".to_string();
-    for key in layer.keys.iter() {
-        let outer_x = keyboard_border + key.x * outer_w;
-        let outer_y = keyboard_border + key.y * outer_w;
-
-        let inner_x = keyboard_border + key.x * outer_w + border_w;
-        let inner_y = keyboard_border + key.y * outer_w + border_top;
-
-        let key_opts = render_opts.get(&layer.id.0, &key.id.0);
-
-        let class = key_opts.class;
-
-        let outer_color = render_opts.colors.get(&class).unwrap_or(&fallback_color);
-
-        let inner_color = lighten_color(Srgb::from_str(outer_color).unwrap().into(), 0.1);
-        let inner_color = format!("#{:x}", Srgb::<u8>::from(inner_color));
-
-        writeln!(
-            file,
-            r##"    <g class="keycap {class}">
-      <rect x="{outer_x}" y="{outer_y}"
-            width="{outer_w}" height="{outer_w}"
-            rx="5" fill="{outer_color}" class="outer border"/>
-      <rect x="{inner_x}" y="{inner_y}"
-            width="{inner_w}" height="{inner_w}"
-            rx="5" fill="{inner_color}" class="inner border"/>
-"##,
-        )?;
-
-        let text_h = 12.0;
-
-        let text = key_opts.title.lines().collect::<Vec<_>>();
-
-        if !text.is_empty() {
-            let y_offset = (text.len() - 1) as f32 * text_h / 2.0;
-
-            let text_x = inner_x + inner_w / 2.0;
-            let text_y = inner_y + inner_w / 2.0 - y_offset;
-
-            writeln!(
-                file,
-                r#"<text x="{text_x}" y="{text_y}" text-anchor="middle" dominant-baseline="middle" class="main">"#
-            )?;
-
-            for (i, txt) in text.into_iter().enumerate() {
-                let txt = html_escape::encode_safe(&txt);
-                let dy = match i {
-                    0 => 0.0,
-                    _ => text_h,
-                };
-                writeln!(file, r#"<tspan x="{text_x}" dy="{dy}">{txt}</tspan>"#)?;
-            }
-
-            writeln!(file, "</text>")?;
-        }
-
-        if let Some(subtxt) = key_opts.hold_title {
-            let text_x = inner_x + inner_w / 2.0;
-            let text_y = inner_y + inner_w + 6.2;
-
-            writeln!(
-                file,
-                r#"<text x="{text_x}" y="{text_y}" text-anchor="middle" class="sub">{subtxt}</text>"#
-            )?;
-        }
-        writeln!(file, "</g>")?;
-    }
+    write_layer_keys(&mut file, layer, render_opts, keymap_border, key_w)?;
 
     file.write_all("</svg>".as_bytes())?;
 
+    Ok(())
+}
+
+fn write_layer_keys(
+    file: &mut File,
+    layer: &Layer,
+    render_opts: &RenderOpts,
+    keymap_border: f32,
+    key_w: f32,
+) -> Result<()> {
+    let fallback_color = "#e5c494".to_string();
+    for key in layer.keys.iter() {
+        let key_opts = render_opts.get(&layer.id.0, &key.id.0);
+        let class = key_opts.class;
+        let outer_color = render_opts.colors.get(&class).unwrap_or(&fallback_color);
+
+        let outer_x = keymap_border + key.x * key_w;
+        let outer_y = keymap_border + key.y * key_w;
+
+        write_bordered_key(
+            file,
+            outer_x,
+            outer_y,
+            key_w,
+            key_w,
+            &class,
+            outer_color,
+            &key_opts.title,
+            key_opts.hold_title.as_deref(),
+        )?;
+    }
     Ok(())
 }
 
@@ -277,6 +216,81 @@ fn render_combos(combos: &[Combo], render_opts: &RenderOpts, output_dir: &Utf8Pa
         }
         println!();
     }
+
+    Ok(())
+}
+
+#[allow(clippy::too_many_arguments)]
+fn write_bordered_key(
+    file: &mut File,
+    outer_x: f32,
+    outer_y: f32,
+    outer_w: f32,
+    outer_h: f32,
+    class: &str,
+    outer_color: &str,
+    title: &str,
+    hold_title: Option<&str>,
+) -> Result<()> {
+    let border_w = 6.0;
+    let border_top = 4.0;
+
+    let inner_w = outer_w - border_w * 2.0;
+    let inner_h = outer_h - border_w * 2.0;
+
+    let inner_x = outer_x + border_w;
+    let inner_y = outer_y + border_top;
+
+    let inner_color = lighten_color(Srgb::from_str(outer_color).unwrap().into(), 0.1);
+    let inner_color = format!("#{:x}", Srgb::<u8>::from(inner_color));
+
+    writeln!(
+        file,
+        r##"    <g class="keycap {class}">
+      <rect x="{outer_x}" y="{outer_y}"
+            width="{outer_w}" height="{outer_h}"
+            rx="5" fill="{outer_color}" class="outer border"/>
+      <rect x="{inner_x}" y="{inner_y}"
+            width="{inner_w}" height="{inner_h}"
+            rx="5" fill="{inner_color}" class="inner border"/>
+"##,
+    )?;
+
+    let text_h = 12.0;
+
+    let text = title.lines().collect::<Vec<_>>();
+    if !text.is_empty() {
+        let y_offset = (text.len() - 1) as f32 * text_h / 2.0;
+        let text_x = inner_x + inner_w / 2.0;
+        let text_y = inner_y + inner_h / 2.0 - y_offset;
+
+        writeln!(
+            file,
+            r#"<text x="{text_x}" y="{text_y}" text-anchor="middle" dominant-baseline="middle" class="main">"#
+        )?;
+
+        for (i, txt) in text.into_iter().enumerate() {
+            let txt = html_escape::encode_safe(&txt);
+            let dy = match i {
+                0 => 0.0,
+                _ => text_h,
+            };
+            writeln!(file, r#"<tspan x="{text_x}" dy="{dy}">{txt}</tspan>"#)?;
+        }
+
+        writeln!(file, "</text>")?;
+    }
+
+    if let Some(subtxt) = hold_title {
+        let text_x = inner_x + inner_w / 2.0;
+        let text_y = inner_y + inner_w + 6.2;
+
+        writeln!(
+            file,
+            r#"<text x="{text_x}" y="{text_y}" text-anchor="middle" class="sub">{subtxt}</text>"#
+        )?;
+    }
+    writeln!(file, "</g>")?;
 
     Ok(())
 }
